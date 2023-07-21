4041f6545afbf00f13ac3918a141f627017828cc
Pierre Ledoux <Pierre.LEDOUX@metarom.fr>
2023-07-21 10:59:01 +0200

    ProductsIndexheader & style

---
+++ src/components/ProductsIndexHeader.vue
@@ -0,0 +1,33 @@
+<script setup lang="ts">
+interface Props {
+  modelValue: string,
+}
+interface Emits {
+  (e: 'reset'): void,
+  (e: 'update:modelValue', value: string): void
+}
+
+const props = defineProps<Props>()
+const emits = defineEmits<Emits>()
+</script>
+
+<template>
+  <div class="top-bar">
+    <h1>Produits</h1>
+    <div class="button-group">
+      <button type="button" @click="emits('reset')" class="btn btn-outline-error">
+        <span class="material-symbols-outlined">
+          close
+        </span>
+      </button>
+      <input type="text" name="search" id="search" class="form-input" placeholder="Rechercher" :value="props.modelValue"
+        @keyup="emits('update:modelValue', ($event.target as HTMLInputElement)?.value)">
+      <RouterLink :to="{ name: 'products-create' }" class="btn btn-secondary">Nouveau</RouterLink>
+    </div>
+  </div>
+</template>
+
+<style scoped lang="scss">
+@import '../assets/css/media-queries';
+@import '../assets/css/components/productsIndexHeader';
+</style>
--- src/pages/ProductsIndex.vue
+++ src/pages/ProductsIndex.vue
@@ -1,35 +1,22 @@
 <script setup lang="ts">
 import { ref, watch, watchEffect } from 'vue'
+import ProductsIndexHeader from '@/components/ProductsIndexHeader.vue';
 import ProductsIndexGrid from '@/components/ProductsIndexGrid.vue';
-import { generateProducts } from '../seed/products'
 import type { ProductInterface } from '@/types';
+import { generateProducts } from '../seed/products'
 
 const products = ref<ProductInterface[]>([])
 const search = ref('')
 const filteredProducts = ref<ProductInterface[]>([])
 const reset = () => { search.value = '' }
+
 watchEffect(() => products.value = generateProducts())
 watch(search, (newValue) => filteredProducts.value = products.value.filter(e => e.name.match(newValue)))
 </script>
 
 <template>
   <div class="container-xl p-1">
-    <div class="top-bar">
-      <h1>Produits</h1>
-      <div class="button-group">
-        <button type="button" v-show="search" @click="reset" class="btn btn-outline-error">
-          <span class="material-symbols-outlined">
-            close
-          </span>
-        </button>
-        <input type="text" name="search" id="search" class="form-input" placeholder="Rechercher" v-model="search">
-        <RouterLink :to="{ name: 'products-create' }" class="btn btn-secondary">Nouveau</RouterLink>
-      </div>
-    </div>
+    <ProductsIndexHeader @reset="reset" v-model="search" />
     <ProductsIndexGrid :products="products" :search="search" :filtered-products="filteredProducts" />
   </div>
 </template>
-
-<style scoped lang="scss">
-@import '../assets/css/components/productsList.scss';
-</style>
