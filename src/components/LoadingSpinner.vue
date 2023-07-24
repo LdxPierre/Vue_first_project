47d06ef76501f924a1f20be68a6e344764c6bb98
Pierre Ledoux <Pierre.LEDOUX@metarom.fr>
2023-07-24 09:56:12 +0200

    comp Spinner

---
+++ src/components/LoadingSpinner.vue
@@ -0,0 +1,9 @@
+<script setup lang="ts"></script>
+
+<template>
+  <div class="h-100 d-flex justify-content-center align-items-center">
+    <span class="material-symbols-outlined">
+      progress_activity
+    </span>
+  </div>
+</template>
