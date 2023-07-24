ff511d09ffc8b0f16e30133f05208538172429da
Pierre Ledoux <Pierre.LEDOUX@metarom.fr>
2023-07-24 16:42:14 +0200

    add filters

--- src/pages/ProductsIndex.vue
+++ src/pages/ProductsIndex.vue
@@ -1,28 +1,39 @@
 <script setup lang="ts">
 import { computed, defineAsyncComponent, reactive, watchEffect } from 'vue'
 import ProductsIndexHeader from '@/components/ProductsIndexHeader.vue';
+import ProductsIndexFilters from '@/components/ProductsIndexFilters.vue';
 import Loading from '../components/ProductsIndexGridLoading.vue';
-import type { ProductInterface } from '@/types';
 import { generateProducts } from '../seed/products'
+import type { ProductInterface } from '@/types';
+import type { FiltersInterface } from '@/types/FiltersInterface';
+
+interface State { products: ProductInterface[], filters: FiltersInterface }
 
 const ProductsIndexGrid = defineAsyncComponent({
   loader: () => import('../components/ProductsIndexGrid.vue'),
   loadingComponent: Loading
 })
 
-const state = reactive<{ products: ProductInterface[], search: string }>({
+const state = reactive<State>({
   products: [],
-  search: '',
+  filters: {
+    search: '',
+    sort: 'nameAsc',
+    minPrice: 1,
+    maxPrice: 100
+  }
 })
-const filteredProducts = computed(() => state.products.filter(e => e.name.match(state.search)))
-const reset = () => { state.search = '' }
+const filteredProducts = computed(() => state.products.filter(e => e.name.match(state.filters.search) && e.price >= state.filters.minPrice && e.price <= state.filters.maxPrice))
+const reset = () => { state.filters.search = '' }
 
 watchEffect(() => state.products = generateProducts())
 </script>
 
 <template>
   <div class="container-xl">
-    <ProductsIndexHeader @reset="reset" v-model="state.search" />
-    <ProductsIndexGrid :products="state.search ? filteredProducts : state.products" />
+    <ProductsIndexHeader @reset="reset" v-model="state.filters.search" />
+    <ProductsIndexFilters v-model:min-price="state.filters.minPrice" v-model:max-price="state.filters.maxPrice"
+      v-model:sort="state.filters.sort" />
+    <ProductsIndexGrid :products="filteredProducts" />
   </div>
 </template>
