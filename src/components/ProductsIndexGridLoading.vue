dab134ea010ad6d8cb1df64864943490630ce863
Pierre Ledoux <Pierre.LEDOUX@metarom.fr>
2023-07-24 09:50:48 +0200

    products card loading

---
+++ src/components/ProductsIndexGridLoading.vue
@@ -0,0 +1,11 @@
+<template>
+  <div class="products-grid">
+    <div v-for="n in 10" v-bind:key="n" class="card-loading">
+    </div>
+  </div>
+</template>
+
+<style scoped lang="scss">
+@import '../assets/css/media-queries';
+@import '../assets/css/components/productsIndexGrid';
+</style>
