91382977c1d9510b61ffb3ca51f59d95cd057d03
Pierre Ledoux <Pierre.LEDOUX@metarom.fr>
2023-07-26 11:53:38 +0200

    type promise return

--- src/helpers/http/products.ts
+++ src/helpers/http/products.ts
@@ -1,15 +1,16 @@
-export const getProducts = async () => {
+import type { ProductInterface } from '@/types'
+
+export const getProducts = async (): Promise<ProductInterface[]> => {
   const response: Response = await fetch('http://localhost:8080/api/products')
   return await response.json()
 }
 
-export const getProduct = async (id: number | string) => {
+export const getProduct = async (id: number | string): Promise<ProductInterface> => {
   const response: Response = await fetch(`http://localhost:8080/api/products/${id}`)
-
   return await response.json()
 }
 
-export const createProduct = async (data: object) => {
+export const createProduct = async (data: object): Promise<void> => {
   const body = JSON.stringify(data)
   const response: Response = await fetch(`http://localhost:8080/api/products`, {
     method: 'POST',
@@ -21,7 +22,10 @@ export const createProduct = async (data: object) => {
   return await response.json()
 }
 
-export const updateProduct = async (id: number | string, data: object) => {
+export const updateProduct = async (
+  id: number | string,
+  data: object
+): Promise<ProductInterface> => {
   const body = JSON.stringify(data)
   const response: Response = await fetch(`http://localhost:8080/api/products/${id}`, {
     method: 'PATCH',
@@ -33,7 +37,7 @@ export const updateProduct = async (id: number | string, data: object) => {
   return await response.json()
 }
 
-export const deleteProduct = async (id: number | string) => {
+export const deleteProduct = async (id: number | string): Promise<void> => {
   const response: Response = await fetch(`http://localhost:8080/api/products/${id}`, {
     method: 'DELETE'
   })
