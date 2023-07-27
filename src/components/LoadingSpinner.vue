8619dec399ee63c3f30c17d3d0dde6a242f886e8
Pierre Ledoux <Pierre.LEDOUX@metarom.fr>
2023-07-27 14:08:42 +0200

    add 200 ms delay

--- src/components/LoadingSpinner.vue
+++ src/components/LoadingSpinner.vue
@@ -1,7 +1,16 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { onUnmounted, ref } from 'vue';
+
+const timeout = ref<number | undefined>(undefined)
+const showSpinner = ref(false)
+timeout.value = setTimeout(() => {
+  showSpinner.value = true
+}, 200)
+onUnmounted(() => clearTimeout(timeout.value))
+</script>
 
 <template>
-  <div class="h-100 d-flex justify-content-center align-items-center">
+  <div v-if="showSpinner" class="h-100 d-flex justify-content-center align-items-center">
     <span class="material-symbols-outlined spinner">
       progress_activity
     </span>
