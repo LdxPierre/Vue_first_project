d002d3c8ef5c882b7212bae5c9a6be0d53b89f67
Pierre Ledoux <Pierre.LEDOUX@metarom.fr>
2023-07-27 16:54:49 +0200

    add 200ms delay

--- src/components/ProductsIndexGridLoading.vue
+++ src/components/ProductsIndexGridLoading.vue
@@ -1,5 +1,20 @@
+<script setup lang="ts">
+import { onUnmounted, ref } from 'vue';
+
+const timeout = ref<null | number | undefined>(null)
+const show = ref<boolean>(false)
+
+timeout.value = setTimeout(() => {
+  show.value = true
+}, 200)
+
+onUnmounted(() => {
+  timeout.value && clearTimeout(timeout.value)
+})
+</script>
+
 <template>
-  <div class="products-grid">
+  <div v-if="show" class="products-grid">
     <div v-for="n in 10" v-bind:key="n" class="card-loading">
     </div>
   </div>
