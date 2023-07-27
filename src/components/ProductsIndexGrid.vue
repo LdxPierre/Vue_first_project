1cd33cec21dd17be47e87597b61ca62d063d93e8
Pierre Ledoux <Pierre.LEDOUX@metarom.fr>
2023-07-27 15:41:03 +0200

    remove query

--- src/components/ProductsIndexGrid.vue
+++ src/components/ProductsIndexGrid.vue
@@ -1,27 +1,15 @@
 <script setup lang="ts">
-import { getProducts } from '@/helpers/http';
 import ProductsIndexCard from './ProductsIndexCard.vue';
 import type { ProductInterface } from '@/types';
 
 defineProps<{
   products: ProductInterface[],
 }>()
-const emits = defineEmits<{
-  (e: 'setProducts', products: ProductInterface[]): void,
-  (e: 'setError', value: string): void
-}>()
-
-try {
-  const response = await getProducts()
-  emits('setProducts', response)
-} catch (e) {
-  emits('setError', 'Erreur lors du chargement initial des produits');
-}
 </script>
 
 <template>
   <div v-if="products.length" class="products-grid">
-    <ProductsIndexCard v-for="product in products" v-bind:key="product.id" :product="product" />
+    <ProductsIndexCard v-for="product in products" v-bind:key="product._id" :product="product" />
   </div>
   <div v-else class="w-100 h-100 d-flex justify-content-center align-items-center">
     Aucun produit trouvé
